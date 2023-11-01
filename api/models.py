9033255dab15af2a341ab421909f76f5dfe8e74f
kylekirkpatrick0kk <kylekirkpatrick0kk@gmail.com>
2023-10-31 20:24:38 -0700

    initial migration and gitignore

--- api/models.py
+++ api/models.py
@@ -6,4 +6,4 @@ class Journal(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, blank=True, null=True, default='')
     starred = models.BooleanField(default=False)
-    entry = models.TextField(required=True, min_length=1, max_length=250)
+    entry = models.TextField(max_length=250)
