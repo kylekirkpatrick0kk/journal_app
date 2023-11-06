50352cb2425aa6f7973de478da1cf6b78ae053f5
kylekirkpatrick0kk <kylekirkpatrick0kk@gmail.com>
2023-11-06 00:46:47 -0700

    add owner to model, migration

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
-# Create your models here.
-
 class Journal(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=100, blank=True, null=True, default='')
     starred = models.BooleanField(default=False)
-    entry = models.TextField(max_length=250)
+    entry = models.TextField(max_length=2500)
+    owner = models.ForeignKey('auth.User', related_name="journals", on_delete=models.CASCADE, blank=True, null=True)
